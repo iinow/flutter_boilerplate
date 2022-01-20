a139c861a9c63e0d9eb24cdedbc03177752e3485
iinow <iinow@naver.com>
2022-01-20 22:29:52 +0900

    socket io 연결 테스트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+import 'common/utils/websocket.dart';
+
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 // import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 void main() {
@@ -10,7 +17,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     var localizationsDelegates = [
@@ -20,30 +26,25 @@ class MyApp extends StatelessWidget {
     ];
 
     var supportedLocales = [
-      // const Locale('ko', 'KR'),
-      // const Locale('en', 'EN'),
+      const Locale('ko', 'KR'),
+      const Locale('en', 'EN'),
     ];
 
-    var str = 'hello world';
     // var deviceData = MediaQuery.of(context);
     // deviceData.size
 
     return MaterialApp(
       title: 'Localizations Sample App',
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
+      // localizationsDelegates: AppLocalizations.localizationsDelegates,
+      // supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.orange,
+        ),
+        // textTheme: GoogleFonts.emilysCanndyTextTheme()
       ),
+      darkTheme: ThemeData.dark(),
       home: const MyHomePage(
         title: 'Flutter Demo Home Page',
       ),
@@ -55,15 +56,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -72,15 +64,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
   // final _localRenderer = RTCVideoRenderer();
+  // final re = RTCPeerConnection()
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -91,52 +80,35 @@ class _MyHomePageState extends State<MyHomePage> {
       'audio': true,
       'video': {'facingMode': 'user'},
     };
-    // _localRenderer.initialize().then((value) => {print('성공')},
-    //     onError: (error) => {print('error: $error')});
 
-    // Helper.openCamera(mediaConstraints)
-    //     .then((value) => _localRenderer.srcObject = value);
+    var socket = io(
+      'http://192.168.0.5:8080/maps',
+      OptionBuilder().setTransports(['websocket']).build(),
+    );
+    socket.onConnect((_) => log('연결 성공'));
+    socket.onConnectError((data) => log(data));
+    socket.onError((data) => log(data));
+    log('dkdkdkd');
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // final socket = SimpleWebSocket('http://192.168.0.5:8080/maps');
+    // socket.connect();
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '누를 수  이2',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text(AppLocalizations.of(context)!.helloWorld),
+            // Text(AppLocalizations.of(context)!.helloWorld),
             // RTCVideoView(_localRenderer)
           ],
         ),
