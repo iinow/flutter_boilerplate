d14f1ff502c2779b517391b9c4fca6a87dbf9d74
iinow <iinow@naver.com>
2022-02-12 22:52:59 +0900

    injectable 수정

--- lib/common/config/injectable.config.dart
+++ lib/common/config/injectable.config.dart
@@ -8,9 +8,12 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../service/i_room_service.dart' as _i3;
-import '../../service/room/mock_room_service.dart' as _i4;
-import '../../service/room/room_service.dart' as _i5;
+import '../../service/room/mock_room_service.dart' as _i5;
+import '../../service/room/room_service.dart' as _i4;
 
+const String _local = 'local';
+const String _dev = 'dev';
+const String _prod = 'prod';
 const String _mock = 'mock';
 // ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
@@ -18,8 +21,9 @@ const String _mock = 'mock';
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.factory<_i3.IRoomService>(() => _i4.MockRoomService(),
+  gh.factory<_i3.IRoomService>(() => _i4.RoomService(),
+      registerFor: {_local, _dev, _prod});
+  gh.factory<_i3.IRoomService>(() => _i5.MockRoomService(),
       registerFor: {_mock});
-  gh.factory<_i3.IRoomService>(() => _i5.RoomService());
   return get;
 }
--- lib/service/room/room_service.dart
+++ lib/service/room/room_service.dart
@@ -3,7 +3,7 @@ import 'package:flutter_example/models/room.dart';
 import 'package:flutter_example/service/i_room_service.dart';
 import 'package:injectable/injectable.dart';
 
-@Injectable(as: IRoomService)
+@Injectable(as: IRoomService, env: [Profile.local, Profile.dev, Profile.prod])
 class RoomService implements IRoomService {
   @override
   List<Room> findRooms() => [Room(name: 'room1'), Room(name: 'room2')];
